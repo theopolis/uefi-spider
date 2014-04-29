b6ac6bbf2b93f642f1d880440786bd09e4cba425
Teddy Reed <teddy@prosauce.org>
2014-04-28 21:35:07 -0600

    Lenovo/HP spider cleanup and exception case (a few) support.

--- uefispider/spiders/lenovo_spider.py
+++ uefispider/spiders/lenovo_spider.py
@@ -2,6 +2,7 @@
 from uefispider.spiders import UefiSpider
 from scrapy.selector import Selector
 from scrapy.http import Request, FormRequest
+from uefi_firmware.utils import red, blue
 
 from uefispider.items import *
 
@@ -21,6 +22,7 @@ scrapy crawl -a dump=/tmp/spiders LenovoSpider
 
 Requirements:
 innoextract, 7-zip, cabextract, unrar; are all helpful.
+http://constexpr.org/innoextract/files/innoextract-1.4.tar.gz
 
 XML structure:
 Properties->
@@ -67,6 +69,13 @@ class LenovoSpider(UefiSpider):
         "lenovo.com",
     ]
 
+    series_list = [
+        "P014", # Laptops & Tablets
+        #"P013", # Desktop & All-In-Ones
+        #"P022", # Workstations
+        #"P023", # Servers
+    ]
+    
     start_urls = [
         "http://support.lenovo.com/en_US/downloads/default.page"
     ]
@@ -87,9 +96,7 @@ class LenovoSpider(UefiSpider):
         return results        
 
     def parse(self, response):
-        series_list = ["P014"]
-
-        for series in series_list:
+        for series in self.series_list:
             yield Request(url= _search_url([series]), callback= self.parse_series,
                 meta= {"series": series, "dont_merge_cookies": True})
 
@@ -100,11 +107,10 @@ class LenovoSpider(UefiSpider):
 
         ### Now we have a set of subseries IDs.
         for result in results:
-            ###yield Request(url= _search_url([series, result[0]]), 
-            yield Request(url= _search_url([series, "S006"]),
+            yield Request(url= _search_url([series, result[0]]), 
+            #yield Request(url= _search_url([series, "S006"]),
                 callback= self.parse_subseries,
                 meta= {"series": series, "subseries": result[0]})
-            return
 
     def parse_subseries(self, response):
         results = self._get_results(response)
@@ -112,12 +118,11 @@ class LenovoSpider(UefiSpider):
         subseries = response.meta["subseries"]
 
         for result in results:
-            ###yield Request(url = _download_url(series, subseries, result[0]),
-            yield Request(url= _download_url(series, "S006", "SS2500"),
+            yield Request(url = _download_url(series, subseries, result[0]),
+            #yield Request(url= _download_url(series, "S006", "SS2500"),
                 callback= self.parse_product,
                 meta= {"cookiejar": result[1], "item_details": result},
                 dont_filter= True)
-            return
 
     def parse_product(self, response):
         def is_bios_update(name):
@@ -165,11 +170,28 @@ class LenovoSpider(UefiSpider):
         uefi = False
         packages = None
 
+        ### Extract information for the current release from the downloads table.
+        ### This information is NOT repeated in the version table below.
+        downloads = sel.css(".downloadTable").xpath(".//tbody/tr")
+        if len(downloads) == 0:
+            ### Sometimes there is no downloads table!?
+            ### Todo: http://support.lenovo.com/en_US/downloads/detail.page?DocID=DS024464
+            print red("Error: no downloads table found.")
+            return
+
+        binary_url = downloads[0].xpath(".//td")[0].xpath(".//a/@href").extract()[0]
+        notes_url = downloads[1].xpath(".//td")[0].xpath(".//a/@href").extract()[0]
+        date = downloads[0].xpath(".//td")[3].xpath("./text()").extract()[0] 
+
         ### This is ugly!
         tables = sel.css(".v14-header-1")
         for table in tables:
+            if len(table.xpath("./text()")) == 0:
+                ### This may be a server without parsed HTML.
+                continue
             table_name = table.xpath("./text()").extract()[0]
-            if table_name.find("Systems") > -1 and table_name.find("Operating") == -1:
+            if table_name.find("Supported") > -1 and table_name.find("Operating") == -1:
+                ### Could be "Systems"/"System"
                 systems = table
             if table_name.find("Summary of Changes") > -1:
                 changes = table
@@ -179,35 +201,80 @@ class LenovoSpider(UefiSpider):
                 packages = table
 
         if not uefi:
+            ### Documents might be missing UEFI instructions.
+            uefi = (response.body.find("UEFI") >= 0)
+
+        print blue(response.meta["doc_id"])
+        print response.url
+        print "date:", date
+        print "UEFI:", "True" if uefi else red("False")
+        print "True" if systems is not None else red("False"), 
+        print "True" if packages is not None else red("False")
+
+        ### Todo: server pages are not found as UEFI
+        ### http://support.lenovo.com/en_US/downloads/detail.page?DocID=DS032912
+
+        if not uefi:
             ### This is not a UEFI update.
             return
 
-        ### Extract information for the current release from the downloads table.
-        ### This information is NOT repeated in the version table below.
-        downloads = sel.css(".downloadTable").xpath(".//tbody/tr")
-        binary_url = downloads[0].xpath(".//td")[0].xpath(".//a/@href").extract()[0]
-        notes_url = downloads[1].xpath(".//td")[0].xpath(".//a/@href").extract()[0]
-        date = downloads[0].xpath(".//td")[3].xpath("./text()").extract()[0] 
+        ### There are cases where the package table is not found.
+        if packages is None:
+            headers = sel.css("th")
+            for header in headers:
+                table_name = header.xpath("./text()").extract()[0]
+                if table_name.find("Package") >= 0:
+                    packages = header
+                    break
 
+        if packages is None:
+            ### Could not recover.
+            print red("Error: no package list found.")
+            return
 
         systems_list = []
-        systems = systems.xpath("../../../following-sibling::ul")[0].xpath(".//li")
-        for system in systems:
-            systems_list.append(system.xpath("./text()").extract()[0])
+        if systems is None:
+            ### Might not have a correctly formatted table for systems support
+            ### http://support.lenovo.com/en_US/downloads/detail.page?DocID=DS013468
+            cells = sel.xpath("//td/text()").extract()
+            scanning_supported = False
+            for cell in cells:
+                if cell.find("Supported") >= 0 and cell.find("Operating") == -1:
+                    scanning_supported = True
+                if scanning_supported:
+                    if cell[0] != "-":
+                        scanning_supported = False
+                        break
+                    systems_list.append(cell[1:].strip())
+            pass
+        else:
+            systems = systems.xpath("../../../following-sibling::ul")[0].xpath(".//li")
+            for system in systems:
+                systems_list.append(system.xpath("./text()").extract()[0])
+
+        if len(systems_list) == 0:
+            print red("Error: no systems found")
+            return
 
         update_list = []
         updates = packages.xpath("../../..//tr")[1:]
         for i, update in enumerate(updates):
             cells = update.xpath(".//td")
+            if len(cells) < 3:
+                ### Might have a row-span (DocID=DS035105)
+                continue
             ### This format will be X.XX (NAME)
-            version = cells[1].xpath("./text()").extract()[0].split("(")
+            version = cells[1].xpath(".//text()").extract()[0].split("(")
             version = "%s (%s" % (version[0].strip(), version[1].strip()) 
-            release = cells[3]
+            release = cells[-1]
             #print version, release
             if i == 0:
                 update_list.append((version, binary_url, notes_url))
                 continue
             urls = release.xpath(".//a/@href").extract()
+            if len(urls) < 2:
+                ### Could be unreleased (Not release to the web) (DocID=DS029726)
+                urls = ["unknown", "unknown"] 
             update_list.append((version, urls[0], urls[1]))
 
         meta = {
@@ -217,9 +284,7 @@ class LenovoSpider(UefiSpider):
             "url": response.url
         }
 
-        yield Request(url= notes_url,
-            callback= self.parse_notes,
-            meta= meta)
+        yield Request(url= notes_url, callback= self.parse_notes, meta= meta)
 
     def parse_notes(self, response):
         ### This is a text-only document containing the versions and release notes.
@@ -240,13 +305,14 @@ class LenovoSpider(UefiSpider):
             line_num += 1
 
             ### Scan for "Package (ID)", next line is a set of delims, then updates until blank-line
-            if line.find("Package (ID)") >= 0:
+            if line.find("Package") >= 0 and line.find("Issue Date") >= 0:
                 line_num += 1
                 for i in xrange(len(document["updates"])):
                     line = text[line_num]
                     line_num += 1
                     if len(line) == 0:
                         ### Problem!
+                        print red("Warning: no lines left while scanning updates.")
                         break
                     version_info = line.split(" ")
                     dates_list.append(version_info[-1])
@@ -283,13 +349,21 @@ class LenovoSpider(UefiSpider):
             item["version"] = update[0]
             item["bios_url"] = update[1]
             item["notes_url"] = update[2]
-            item["date"] = dates_list[i]
-            item["desc"] = release_notes[i]
+            item["date"] = dates_list[i] if i < len(dates_list) else "unknown"
+            item["desc"] = release_notes[i] if i < len(release_notes) else "unknown"
             item["item_id"] = update[0]
 
-            yield Request(url= item["bios_url"],
-                callback= self.parse_binary,
-                meta= {"item": item})
+            if item["bios_url"] == "unknown":
+                print red("Warning: BIOS url unknown")
+                yield item
+            else:
+                if item["bios_url"][0:len("http://")] != "http://":
+                    ### Might be missing...
+                    ### download.lenovo.com/ibmdl/pub/pc/pccbbs/mobiles/h6uj03ww.exe
+                    item["bios_url"] = "http://" + item["bios_url"]
+                yield Request(url= item["bios_url"],
+                    callback= self.parse_binary,
+                    meta= {"item": item})
         pass
 
     def parse_binary(self, response):
